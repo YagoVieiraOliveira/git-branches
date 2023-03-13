f8239ef5c3739338f0d3672f121e38f1605e20b4
YagoVieiraOliveira <124629602+YagoVieiraOliveira@users.noreply.github.com>
2023-03-13 18:01:26 -0300

    arrumando o cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,12 +1,9 @@
 public class Cliente {
-<<<<<<< HEAD
     private String nome;
     private String cpf;
     private int agencia;
     private double saldo;
     private String tipoConta;
 
-=======
     private String tipoConta;
->>>>>>> a83150b2b7370a7cfb97a1ddbcb31f6a2d2318c7
 }
